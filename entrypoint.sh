b943e673a65f96319536bb970016db888abf19ca
Denis Baryshev <dennybaa@gmail.com>
2018-10-25 15:19:27 +0300

    fixed container invocation
    Signed-off-by: Denis Baryshev <dennybaa@gmail.com>

--- entrypoint.sh
+++ entrypoint.sh
@@ -3,17 +3,17 @@ set -e
 
 # first arg is `-f` or `--some-option`
 if [ "${1#-}" != "$1" ]; then
-    set -- traefik "$@"
+    set -- "$@"
 fi
 
 # if our command is a valid Traefik subcommand, let's invoke it through Traefik instead
 # (this allows for "docker run traefik version", etc)
 if traefik "$1" --help | grep -s -q "help"; then
-    set -- traefik "$@"
+    set -- "$@"
 fi
 
 # start cron deaemon
 /usr/sbin/crond -L /dev/null
 
 # start traefik
-exec "$@"
+exec traefik "$@"
